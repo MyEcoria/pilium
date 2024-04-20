ef34cd41eb4f9d2e1132014ca7aae92bfd12b009
MyEcoria <contact@myecoria.com>
2024-04-20 13:02:52 +0200

    update url

--- lib/network/account_service.dart
+++ lib/network/account_service.dart
@@ -40,9 +40,9 @@ import 'package:natrium_wallet_flutter/network/model/response/process_response.d
 import 'package:natrium_wallet_flutter/bus/events.dart';
 
 // Server Connection String
-const String _SERVER_ADDRESS = "wss://pilium.raione.cc";
-const String _SERVER_ADDRESS_HTTP = "https://pilium.raione.cc/api";
-const String _SERVER_ADDRESS_ALERTS = "https://pilium.raione.cc/alerts";
+const String _SERVER_ADDRESS = "wss://pilium.myecoria.com";
+const String _SERVER_ADDRESS_HTTP = "https://pilium.myecoria.com/api";
+const String _SERVER_ADDRESS_ALERTS = "https://pilium.myecoria.com/alerts";
 
 Map decodeJson(dynamic src) {
   return json.decode(src);
